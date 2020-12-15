07e9c489317bd9f1e0636b6fff69405dccfc6646
Sameer Vohra <vsameer@vmware.com>
2020-12-14 19:06:20 -0600

    [#4578] Update imc-dispatcher
    - trace should include Open Telemetry Spec attributes (messaging.*)
      messaging.destination
      messaging.message_id
      messaging.protocol
      messaging.system

---
+++ pkg/tracing/populate_span_transformer.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tracing
+
+import (
+	"github.com/cloudevents/sdk-go/v2/binding"
+	"github.com/cloudevents/sdk-go/v2/binding/spec"
+	"github.com/cloudevents/sdk-go/v2/types"
+	"go.opencensus.io/trace"
+)
+
+func PopulateSpan(span *trace.Span, destination string) binding.TransformerFunc {
+	return func(reader binding.MessageMetadataReader, writer binding.MessageMetadataWriter) error {
+		span.AddAttributes(MessagingProtocolHTTP)
+		span.AddAttributes(MessagingSystemAttribute)
+
+		_, id := reader.GetAttribute(spec.ID)
+		if id != nil {
+			idParsed, err := types.Format(id)
+			if err != nil {
+				return err
+			}
+			span.AddAttributes(trace.StringAttribute(MessagingMessageIDAttributeName, idParsed))
+		}
+
+		span.AddAttributes(trace.StringAttribute(MessagingDestinationAttributeName, destination))
+
+		return nil
+	}
+}
